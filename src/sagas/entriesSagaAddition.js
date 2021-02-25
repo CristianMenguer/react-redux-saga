d4317d3580cc62dafb3ae0f66dd2f7825cc4f7de
Cristian Menguer <cristianmenguer@gmail.com>
2021-02-25 15:26:48 +0000

    add entries

--- src/sagas/entriesSagaAddition.js
+++ src/sagas/entriesSagaAddition.js
@@ -1,30 +1,29 @@
-import { call, put, take } from "redux-saga/effects"
+import { call, put, takeLatest } from "redux-saga/effects"
 import { types as entriesTypes } from '../Redux/actions/entries.actions'
 import axios from "axios"
 
 export function* addEntrySaga() {
-    while (true) {
-        const removal = yield take(entriesTypes.ADD_ENTRY)
-        //
-        const response = yield call(addEntry, removal.payload)
-        if (response === 201) {
-            console.log(removal.payload)
-            yield put({ type: entriesTypes.ADD_ENTRY_RESULT, payload: { payload: removal.payload } })
-        }
-    }
+    yield takeLatest(entriesTypes.ADD_ENTRY, addEntryToDb)
+}
+
+function* addEntryToDb({ payload }) {
+    yield call(addEntry, payload)
+    yield call(addEntryDetails, payload)
+    yield put({ type: entriesTypes.ADD_ENTRY_RESULT, payload })
 
 }
 
-const addEntry = async (entry) => {
+async function addEntryDetails(payload) {
     await axios.post('http://localhost:3003/entriesValues/', {
-        id: entry.id,
-        value: entry.value,
-        isIncome: entry.isIncome
+        id: payload.id,
+        value: payload.value,
+        isIncome: payload.isIncome
     })
-    const response = await axios.post('http://localhost:3003/entries/', {
-        id: entry.id,
-        description: entry.description
+}
+
+async function addEntry(payload) {
+    await axios.post('http://localhost:3003/entries/', {
+        id: payload.id,
+        description: payload.description
     })
-    return response.status
-    // return 200
 }
